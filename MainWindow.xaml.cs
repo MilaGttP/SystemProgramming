df619955f7e9c8cf9d51acf69129cf96e5720547
Mila <milaivanova2609@gmail.com>
2023-01-02 15:25:16 +0200

    Initial

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,14 +15,35 @@ using System.Windows.Shapes;
 
 namespace SystemProgramming
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Task1_Click(object sender, RoutedEventArgs e)
+        {
+            Task1 task1 = new Task1();
+            task1.ShowDialog();
+        }
+
+        private void Task2_Click(object sender, RoutedEventArgs e)
+        {
+            Task2 task2 = new Task2();
+            task2.ShowDialog();
+        }
+
+        private void Task3_Click(object sender, RoutedEventArgs e)
+        {
+            Task3 task3 = new Task3();
+            task3.ShowDialog();
+        }
+
+        private void Task4_Click(object sender, RoutedEventArgs e)
+        {
+            Task4 task4 = new Task4();
+            task4.ShowDialog();
         }
     }
 }
