0c4197c9abea0fb03948678290d8c366a65f7e97
Mila <milaivanova2609@gmail.com>
2023-01-02 19:30:59 +0200

    Initial

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SystemProgramming
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Task1_Click(object sender, RoutedEventArgs e)
+        {
+            Task1 task1 = new Task1();
+            task1.ShowDialog();
+        }
+
+        private void Task2And3_Click(object sender, RoutedEventArgs e)
+        {
+            Task2And3 task2And3 = new Task2And3();
+            task2And3.ShowDialog();
+        }
+
+        private void Task4And5_Click(object sender, RoutedEventArgs e)
+        {
+            Task4And5 task4And5 = new Task4And5();
+            task4And5.ShowDialog();
         }
     }
 }
