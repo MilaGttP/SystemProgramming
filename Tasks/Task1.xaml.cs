6b3a3f749134d3ba3b4e2e27185d19687f9f935f
Mila <milaivanova2609@gmail.com>
2023-01-02 17:05:53 +0200

    Tasks 1, 2 completed

--- Tasks/Task1.xaml.cs
+++ Tasks/Task1.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,14 +15,16 @@ using System.Windows.Shapes;
 
 namespace SystemProgramming
 {
-    /// <summary>
-    /// Логика взаимодействия для Task1.xaml
-    /// </summary>
     public partial class Task1 : Window
     {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
         public Task1()
         {
             InitializeComponent();
+            MessageBox(new IntPtr(0), "Ivanova Mila", "Full name", 0);
+            MessageBox(new IntPtr(0), "16", "Age", 0);
+            MessageBox(new IntPtr(0), "Math, programming", "Favorite subjects", 0);
         }
-    }
+    }        
 }
--- Tasks/Task2.xaml.cs
+++ Tasks/Task2.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,20 +9,46 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace SystemProgramming
 {
-    /// <summary>
-    /// Логика взаимодействия для Task2.xaml
-    /// </summary>
     public partial class Task2 : Window
     {
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern Int32 FindWindow(String lpClassName, String lpWindowName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr SendMessage(IntPtr hwnd, uint Msg, int wParam, String lParam);
+        public int GetWindowId(string className, string windowName)
+        {
+            return FindWindow (className, windowName);
+        }
+        public const int WM_USER = 0x400;
+        public const int WM_COPYDATA = 0x4A;
+       
         public Task2()
         {
             InitializeComponent();
+        }
+
+        private void CaptionTB_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                this.Title = CaptionTB.Text;
+                int hWnd = GetWindowId(null, "Task2");
+                SendMessage(hWnd, WM_COPYDATA, 0, $"Caption changed on {CaptionTB.Text}!");
+                //MessageBox.Show($"Caption changed on {CaptionTB.Text}!");
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBox.Show("Closed!");
         }
     }
 }
