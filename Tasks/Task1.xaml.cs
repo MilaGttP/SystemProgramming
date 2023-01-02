e22e0e2a8b4a6924b479bc3a38e2fd52b5330ee0
Mila <milaivanova2609@gmail.com>
2023-01-02 20:59:35 +0200

    1

--- Tasks/Task1.xaml.cs
+++ Tasks/Task1.xaml.cs
@@ -11,17 +11,25 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Threading;
 
 namespace SystemProgramming
 {
-    /// <summary>
-    /// Логика взаимодействия для Task1.xaml
-    /// </summary>
     public partial class Task1 : Window
     {
         public Task1()
         {
             InitializeComponent();
+        }
+        private void StartProc_Click(object sender, RoutedEventArgs e)
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = "calc.exe";
+            process.Start();
+            StartProc.Content = "Процес розпочато!";
+            process.WaitForExit();
+            StartProc.Content = $"Процес завершився з кодом: {process.ExitCode}";
         }
     }
 }
