5064e27e3fed5cde438563796726761352b84ee9
Mila <milaivanova2609@gmail.com>
2023-01-14 15:46:31 +0200

    Updated

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,22 +9,6 @@ namespace SystemProgramming
         {
             InitializeComponent();
         }
-        private void Task1_Click(object sender, RoutedEventArgs e)
-        {
-            Task1 task1 = new Task1();
-            task1.ShowDialog();
-        }
-
-        private void Task2And3_Click(object sender, RoutedEventArgs e)
-        {
-            Task2And3 task2And3 = new Task2And3();
-            task2And3.ShowDialog();
-        }
-
-        private void Task4And5_Click(object sender, RoutedEventArgs e)
-        {
-            Task4And5 task4And5 = new Task4And5();
-            task4And5.ShowDialog();
-        }
+        
     }
 }
