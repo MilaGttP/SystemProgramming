02bca6762b3f5ee934efa255c1526e76261b96fc
Mila <milaivanova2609@gmail.com>
2023-01-16 20:52:33 +0200

    Initital

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,11 +15,28 @@ using System.Windows.Shapes;
 
 namespace SystemProgramming
 {
+    public interface ISwitchable { void UtilizeState(object state); }
+    public static class Switcher
+    {
+        public static MainWindow pageSwitcher;
+        public static void Switch(UserControl newPage) => pageSwitcher.Navigate(newPage);
+        public static void Switch(UserControl newPage, object state) => pageSwitcher.Navigate(newPage, state);
+    }
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            Switcher.pageSwitcher = this;
+            Switcher.Switch(new Main());
+        }
+        public void Navigate(UserControl nextPage) => this.Content = nextPage;
+        public void Navigate(UserControl nextPage, object state)
+        {
+            this.Content = nextPage;
+            ISwitchable s = nextPage as ISwitchable;
+            if (s != null) s.UtilizeState(state);
+            else throw new ArgumentException("NextPage is not ISwitchable! " + nextPage.Name.ToString());
         }
     }
 }
---
+++ Tasks/Main.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SystemProgramming
+{
+    public partial class Main : UserControl
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        private void Task1Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Task1());
+        }
+
+        private void Task2Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Task2());
+        }
+
+        private void Task3Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Task3());
+        }
+
+        private void Task4Btn_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Task4());
+        }
+    }
+}
