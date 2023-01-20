7055c4c617163a4eb76f573c8b7c18ef39b31c86
Mila <101627933+MilaGttP@users.noreply.github.com>
2023-01-20 21:39:29 +0200

    Add files via upload

---
+++ DLLs/FactorialSimple.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyLoadLibrary
+{
+    public static class FactorialSimple
+    {
+        public static int Factorial(int number)
+        {
+            int i, fact = 1;
+            for (i = 1; i <= number; i++) fact = fact * i;
+            return fact;
+        }
+        public static bool IsPrime(int number)
+        {
+            int i, m = 0, flag = 0;
+            m = number / 2;
+            for (i = 2; i <= m; i++)
+            {
+                if (number % i == 0)
+                {
+                    flag = 1;
+                    return false;
+                }
+                else return true;
+            }
+            if (flag == 0) return true;
+            else return false;
+        }
+        public static bool IsEven(int number)
+        {
+            if (number % 2 == 0) { return true; }
+            return false;
+        }
+        public static bool IsOdd(int number)
+        {
+            if (number % 2 != 0) { return true; }
+            return false;
+        }
+    }
+}
---
+++ DLLs/Fraction.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssemblyLoadLibrary
+{
+    public class Fraction
+    {
+        public int numerator { get; set; }
+        public int denominator { get; set; }
+
+        public Fraction()
+        {
+            numerator= 0;
+            denominator= 1;
+        }
+        public Fraction(int numerator, int denominator)
+        {
+            if (denominator == 0) throw new DivideByZeroException();
+            this.numerator = numerator;
+            this.denominator = denominator;
+        }
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            int r1 = (int)a.numerator * b.denominator + (int)b.numerator * a.denominator;
+            int r2 = (int)a.denominator * b.denominator;
+            return new Fraction(r1, r2);
+        }
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            int r1 = (int)a.numerator * b.denominator - (int)b.numerator * a.denominator;
+            int r2 = (int)a.denominator * b.denominator;
+            return new Fraction(r1, r2);
+        }
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            int r1 = (int)a.numerator * b.numerator;
+            int r2 = (int)a.denominator * b.denominator;
+            return new Fraction(r1, r2);
+        }
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            int r1 = (int)a.numerator * b.denominator;
+            int r2 = (int)a.denominator * b.numerator;
+
+            if (r2 == 0) throw new DivideByZeroException();
+            else return new Fraction(r1, r2);
+        }
+    }
+}
