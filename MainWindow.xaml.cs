0f9b58bfa97061ab7806a25bcea69eec07c8a545
Mila <milaivanova2609@gmail.com>
2023-01-20 18:45:51 +0200

    Initial

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,5 +21,28 @@ namespace SystemProgramming
         {
             InitializeComponent();
         }
+        private void Task1_Click(object sender, RoutedEventArgs e)
+        {
+            Task1 task1 = new Task1();
+            task1.ShowDialog();
+        }
+
+        private void Task2_Click(object sender, RoutedEventArgs e)
+        {
+            Task2 task2 = new Task2();
+            task2.ShowDialog();
+        }
+
+        private void Task3_Click(object sender, RoutedEventArgs e)
+        {
+            Task3 task3 = new Task3();
+            task3.ShowDialog();
+        }
+
+        private void Task4_Click(object sender, RoutedEventArgs e)
+        {
+            Task4 task4 = new Task4();
+            task4.ShowDialog();
+        }
     }
 }
